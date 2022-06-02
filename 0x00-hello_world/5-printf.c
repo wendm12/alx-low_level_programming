8b4ee7e58e700d9cfd2252450227b017ec85b05e
wendm12 <a.wendmagegn12@gmail.com>
2022-06-02 14:25:17 -0700

    5-printf new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-/*function main begins program execuction*/
+/*function main 
+ *begins program
+ *execuction
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
