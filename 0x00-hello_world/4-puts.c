a27afc2c2813d2a8093607ad827f227f755a7cdb
wendm12 <a.wendmagegn12@gmail.com>
2022-06-02 13:30:34 -0700

    rev hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include<string.h>
 
-int main()
+int main(void)
 {
 	char string[100];
-	strcpy(string, "\"programming is like building a multilingual puzzle");
+	strcpy(string, "\"programming is like building a multilingual puzzle\n");
 	puts(string);
-	return 0;
+	return (0);
 }
