13e2ee9b89514ca3d7c74640e1b5310816f07679
wendm12 <a.wendmagegn12@gmail.com>
2022-06-04 07:45:37 -0700

    2 print alphabet rev2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,14 @@
 /*alx software ingineering*/
 #include<stdio.h>
-/*main:print the alphabet in lowercase, followed by a new line */
+/*
+ * main:print the alphabet in lowercase, followed by a new line
+ */
 int main(void)
 
 {
 	int ch;
 	for (ch =  'a'; ch <= 'z'; ch++)
+	
 	{
 		putchar(ch);
 	}
