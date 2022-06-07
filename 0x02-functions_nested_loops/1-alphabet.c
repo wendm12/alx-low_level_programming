9fa73093480a38b67a3f2b91bcf7a928f9c5f31d
wendm12 <a.wendmagegn12@gmail.com>
2022-06-07 13:18:36 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ *main-alphabet in lowercase
+ *Return:Always 0
+ */
+void print_alphabet(void);
+/*alphabet in lowercase*/
+int main(void)
+{
+	int i;
+	for (i = 'a' ;i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	 	putchar('\n');
+		return (0);
+}
+
