380cf738511ba9ea4ebb1243068b25b96fd73f98
wendm12 <a.wendmagegn12@gmail.com>
2022-06-08 13:36:14 -0700

    sign rev6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,16 +10,16 @@ int print_sign(int n)
 	{
 		
 		putchar('+');
-	       	putchar('1');
+	       	return('1');
 	}
 	else if (n == 0)
 	{
-		putchar(0);
-		return(0);
+		putchar('0');
+		return('0');
 	}
 	else 
 	{
 		putchar('-');
-		return(-1);
+		return('1');
 	}
 }
