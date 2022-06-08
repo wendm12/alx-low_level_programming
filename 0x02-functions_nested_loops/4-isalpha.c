f18e383174b3e5b480f76e8670d0ae1542cc8a1a
wendm12 <a.wendmagegn12@gmail.com>
2022-06-08 10:52:51 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+ /**
+  * _isalpha-print 1 if letter 0 if not
+  *Return: 1 or 2
+  */
+int _isalpha(int c)
+{
+	if ( c = 'a' ; c <= 'z' ; c++)
+	{	
+		return (1);
+	}
+	else if (c = 'A' ; c <= 'Z' ; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
