71ac701297c750d7a7676aed53bf3b2d84241fa8
wendm12 <a.wendmagegn12@gmail.com>
2022-06-08 06:45:05 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ *main-print alphabet x10
+ *Return:0
+ */
+void print_alphabet_x10(void)/*defination of main function print alphabet x10*/
+{
+	int i=0;
+	int j;
+	while (i < 10)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		{
+			putchar(j);
+		}
+		putchare('\n');
+		i++;
+	}
+	return;
+}
+
