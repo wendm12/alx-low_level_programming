8abb005261fb1662c67cf61090ccb64c84b53dea
wendm12 <a.wendmagegn12@gmail.com>
2022-06-08 01:25:51 -0700

    alphabet new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *main-alphabet in lowercase
  *Return:Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 /*alphabet in lowercase*/
-int main(void)
 {
 	int i;
 	for (i = 'a' ;i <= 'z' ; i++)
