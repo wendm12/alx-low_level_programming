ce7062fa1cedaadd377c9e4485f52344f89e91d3
wendm12 <a.wendmagegn12@gmail.com>
2022-06-10 12:54:00 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main-test function that prints if integer is positive or negative
+ *Return: 0
+ */
+int main(void)
+{
+	positive_or_negative(10);
+	positive_or_negative(0);
+	positive_or_negative(-5);
+	positive_or_negative(4);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+debugging  and reading error messages
