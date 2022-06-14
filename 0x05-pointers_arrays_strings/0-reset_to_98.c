967461eeaa5220cbe3c822c1e605bc2669ca7c82
wendm12 <a.wendmagegn12@gmail.com>
2022-06-14 11:28:30 -0700

    98battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*tack a pointer to an int as parameter and update the value*/
+ 
+ /*function reser_to_98-yack a pointer to an int as parameter and the value*/
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *p;
+	p = &n;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+pointers and arrays strings
